53633e50ce615ba47361acaa83f9b8709e36631f
AgataPAMQuites <agatapq@hotmail.com>
2022-12-18 02:15:10 -0300

    update 01 17.12

---
+++ Desafios/CriandoBancoDigital/src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    
+    public static void main(String [] args){
+
+        Cliente Agata = new Cliente();
+        Agata.setNome("Agata PAM Quites");
+
+        Cliente Diego = new Cliente();
+        Diego.setNome("Diego P D");
+
+        Conta ccA = new ContaCorrente(Agata);
+        Conta ccD = new ContaPoupanca(Diego);
+
+        ccA.imprimirExtrato();
+        ccD.imprimirExtrato();
+    }
+}
